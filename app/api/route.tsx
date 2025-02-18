3382c762c0bc43e4bd3a6250614cfed8b56759d7
gnomadic <edflem+github@gmail.com>
2025-02-18 12:32:18 -0500

    csv ingestion and filtering, filtering unused for now

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,4 +1,8 @@
+
 import { NextRequest, NextResponse } from 'next/server';
+
+const request = require('request')
+const csv = require('csvtojson')
 
 const data = [
     { id: 1, name: 'Item 1', description: 'This is item 1' },
@@ -6,15 +10,24 @@ const data = [
     { id: 3, name: 'Item 3', description: 'This is item 3' },
 ];
 
-export async function GET(request: NextRequest) {
+export async function GET(req: NextRequest) {
 
-    const week = request.nextUrl.searchParams.get('week');
+    const level = req.nextUrl.searchParams.get('level');
 
-    if (!week || week === "undefined") {
-        return NextResponse.json({ "error": "please provide a week parameter" }, { status: 400 });
+    if (!level || level === "undefined") {
+        return NextResponse.json({ "error": "please provide a level parameter" }, { status: 400 });
     }
 
-    return NextResponse.json({ "cool": `coolio week: ${week}` }, { status: 200 });
+    const jsonArray = await csv().fromStream(request.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRxSPB3HP9zCiD7gOWuzDiGX1fWLAWEvm8Dyj1tVQ34KjgFGxLW0jGxZXH89ItBH99lAkHt08yu-uHY/pub?gid=0&single=true&output=csv'));
+
+    const levelFiltered = jsonArray.filter((item: any) => item.level == level);
+
+    return NextResponse.json(
+        {
+            "level": `${level}`,
+            "data": jsonArray
+        }
+        , { status: 200 });
 
 
 }
